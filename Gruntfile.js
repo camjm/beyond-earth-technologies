096deaa51b6488d1dc869d5ac810aa2eb95adbd6
Cam <cam@mackay.net.nz>
2015-06-28 21:42:40 +1200

    grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,27 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jade');
 
+    //TODO: correct name of task
     grunt.registerMultiTask('blah', 'blah blah', function() {
-
+        //TODO: implement task
     });
+
+    //TODO: jshint task for json data?
+
+    //TODO: search for task to generate many jade files from one
+    //http://stackoverflow.com/questions/24559380/creating-multiple-html-files-from-a-single-jade-template-with-grunt
+    //http://stackoverflow.com/questions/17798358/grunt-compiling-jade-files
+    //https://github.com/gruntjs/grunt-contrib-jade
 
     // Project configuration
     grunt.initConfig({
         clean: {
             src: ['build']
+        },
+        blah: {
+            technologies: {
+                data: "data/technologies.json"
+            }
         },
         jade: {
             options: {
